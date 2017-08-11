af229bc33e3cadd600a8c26ef81fbc9a3013b4a9
Jonny Anderson <Jonny@JonnyAnderson.com>
2017-08-10 22:51:02 -0600

    Customize 404 page and add registry/rsvp redirects

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,21 @@
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home')->name('home');
 });
 
 Route::get('/ido/registry', function () {
 	return redirect('https://www.amazon.com/wedding/share/jolly');
+})->name('wedding.registry');
+
+Route::get('/registry', function () {
+	return redirect()->route('wedding.registry');
 });
 
 Route::get('/ido/rsvp', function () {
     return redirect('https://docs.google.com/forms/d/e/1FAIpQLSd_WazCjrINn0ljIVKDALHZqPyesEapTlv4gHutt9McxnDT_A/viewform?usp=sf_link');
+})->name('wedding.rsvp');
+
+Route::get('/rsvp', function () {
+	return redirect()->route('wedding.rsvp');
 });
